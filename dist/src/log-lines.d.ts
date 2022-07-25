629c6990839c437efb08f325fc77d74ded6cce9f
Eren Kara <erenkyi0@gmail.com>
2022-07-25 15:57:25 +0300

    build

--- dist/src/log-lines.d.ts
+++ dist/src/log-lines.d.ts
@@ -64,7 +64,7 @@ export declare class LogDamage extends LogLine {
     targetId: string;
     targetName: string;
     damage: number;
-    damageModifier: boolean;
+    damageModifier: number;
     isCrit: boolean;
     isBackAttack: boolean;
     isFrontAttack: boolean;
--- dist/util.module.js
+++ dist/util.module.js
@@ -1,2 +1,2 @@
-function r(r,t){if(void 0===t&&(t=0),"number"==typeof r)return isNaN(r)?t:r;var e;try{e=parseInt(r),isNaN(e)&&(e=t)}catch(r){e=t}return e}export{r as tryParseInt};
+function r(r,t,e){if(void 0===t&&(t=0),void 0===e&&(e=10),"number"==typeof r)return isNaN(r)?t:r;var i;try{i=parseInt(r,e),isNaN(i)&&(i=t)}catch(r){i=t}return i}export{r as tryParseInt};
 //# sourceMappingURL=util.module.js.map
